46d82cfaad9229765c3543e594d738c4cbbeb7ea
a <a@as-Virtual-Machine.local>
2026-09-09 17:02:23 -0700

    Enable gitleaks pre‑commit hook with custom entry command

--- devenv.nix
+++ devenv.nix
@@ -11,7 +11,10 @@
   env.GREET = "devenv";
 
   # https://devenv.sh/packages/
-  packages = [ pkgs.git ];
+  packages = [
+    pkgs.git
+    pkgs.gitleaks
+  ];
 
   # https://devenv.sh/languages/
   # languages.rust.enable = true;
@@ -49,6 +52,12 @@
   # git-hooks.hooks.shellcheck.enable = true;
   git-hooks.hooks = {
     end-of-file-fixer.enable = true;
+    gitleaks = {
+      enable = true;
+      # https://github.com/gitleaks/gitleaks/blob/b58d3f102cf3a2c84cb7f923d05c25c9b1aed84b/.pre-commit-hooks.yaml#L4
+      # Direct execution of gitleaks here results in '[git] fatal: cannot change to 'devenv.nix': Not a directory'.
+      entry = "bash -c 'exec gitleaks git --redact --staged --verbose'";
+    };
     nixfmt.enable = true;
     prettier = {
       enable = true;
